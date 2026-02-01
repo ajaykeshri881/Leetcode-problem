a8084731cfc568b9d76fde859bd5b59e2c24f014
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-01 17:13:30 +0530

    Time: 1 ms (99.20%) | Memory: 45.6 MB (30.80%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,19 +1,32 @@
+// class Solution {
+//     public int minimumCost(int[] nums) {
+//         int n = nums.length;
+
+//         int min1 = Integer.MAX_VALUE;
+//         int min2 = Integer.MAX_VALUE;
+//         for (int i = 1; i < n; i++) {
+//             int x = nums[i];
+//             if (x < min1) {
+//                 min2 = min1;
+//                 min1 = x;
+//             } else if (x < min2) {
+//                 min2 = x;
+//             }
+//         }
+
+//         return nums[0] + min1 + min2;
+//     }
+// }
 class Solution {
     public int minimumCost(int[] nums) {
         int n = nums.length;
-
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
-        for (int i = 1; i < n; i++) {
-            int x = nums[i];
-            if (x < min1) {
-                min2 = min1;
-                min1 = x;
-            } else if (x < min2) {
-                min2 = x;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i <= n - 2; i++) {
+            for (int j = i + 1; j <= n - 1; j++) {
+                int cost = nums[0] + nums[i] + nums[j];
+                ans = Math.min(ans, cost);
             }
         }
-
-        return nums[0] + min1 + min2;
+        return ans;
     }
 }
