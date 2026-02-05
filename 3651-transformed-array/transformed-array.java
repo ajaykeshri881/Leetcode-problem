1f77ca46b7f5799030c222db1c72b11b9a2a8643
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-05 16:20:43 +0530

    Time: 2 ms (1.28%) | Memory: 46.8 MB (51.92%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int steps = nums[i];
+            if (steps == 0) {
+                result[i] = 0;
+                continue;
+            }
+
+            int cur = i;
+
+            if (steps > 0) {
+                for (int k = 0; k < steps; k++) {
+                    cur = (cur + 1) % n;
+                }
+            } else {
+                for (int k = 0; k < -steps; k++) {
+                    cur = (cur - 1 + n) % n;
+                }
+            }
+
+            result[i] = nums[cur];
+        }
+
+        return result;
+    }
+}
