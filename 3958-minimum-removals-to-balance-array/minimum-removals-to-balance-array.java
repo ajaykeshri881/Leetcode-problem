65306177c167f5db2cb2da952322d51df3f616e5
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-06 15:01:08 +0530

    Time: 26 ms (21.29%) | Memory: 86.3 MB (36.95%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int l = 0, max = 1;
+
+        for (int r = 0; r < n; r++) {
+            while ((long) nums[r] > (long) nums[l] * k) {
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+
+        return n - max;
+    }
+}
