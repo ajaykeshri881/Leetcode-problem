ff26d0990483cfef78f4d1d0f2fcd189063e377e
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-15 02:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (88.63%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,5 +1,37 @@
+// class Solution {
+//     public boolean isStrictlyPalindromic(int n) {
+//         return false;
+//     }
+// }
+
 class Solution {
+
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for (int base = 2; base <= n - 2; base++) {
+            String converted = toBase(n, base);
+            if (!isPalindrome(converted)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String toBase(int num, int base) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(num % base);
+            num /= base;
+        }
+        return sb.reverse().toString();
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
