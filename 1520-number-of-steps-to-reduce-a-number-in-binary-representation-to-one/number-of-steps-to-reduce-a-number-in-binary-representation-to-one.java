74f8642b080bda4939784c6f87ee0b433ac377d7
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-26 16:27:17 +0530

    Time: 4 ms (21.83%) | Memory: 43.1 MB (25.07%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,28 +1,56 @@
+// class Solution {
+//     public int numSteps(String s) {
+//         int l = s.length() - 1;
+//         int carry = 0;
+//         int count = 0;
+        
+//         while (l > 0) {
+//             if (Character.getNumericValue(s.charAt(l)) + carry == 0) {
+//                 carry = 0;
+//                 count++;
+//             } else if (Character.getNumericValue(s.charAt(l)) + carry == 2) {
+//                 carry = 1;
+//                 count++;
+            
+//             } else {
+//                 carry = 1;
+//                 count += 2;
+//             }
+//             l--;
+//         }
+      
+//         if (carry == 1) {
+//             count++;
+//         }
+        
+//         return count;
+//     }
+// }
+
 class Solution {
     public int numSteps(String s) {
-        int l = s.length() - 1;
-        int carry = 0;
-        int count = 0;
-        
-        while (l > 0) {
-            if (Character.getNumericValue(s.charAt(l)) + carry == 0) {
-                carry = 0;
-                count++;
-            } else if (Character.getNumericValue(s.charAt(l)) + carry == 2) {
-                carry = 1;
-                count++;
-            
-            } else {
-                carry = 1;
-                count += 2;
+        Stack<Integer> st=new Stack<>();
+        for(char c:s.toCharArray()) st.push(c-'0');
+        int carry=0;
+        int steps=0;
+        while(st.size()>1){
+            if(st.peek()==1&&carry==1){ 
+                st.pop();
+                steps++;
+            }else if(st.peek()==1){ 
+                st.pop();
+                steps+=2;
+                carry=1;
+            }else if(st.peek()==0&&carry==1){
+                st.pop();
+                st.push(1);
+                carry=0;
+            }else{
+                st.pop();
+                steps++;
             }
-            l--;
         }
-      
-        if (carry == 1) {
-            count++;
-        }
-        
-        return count;
+        if(carry==1) steps++;
+        return steps;
     }
 }
