801088ee5f233058a8267630d2705ee90352cbf0
Ajay Keshri <ajaykeshri881@gmail.com>
2026-02-26 16:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (63.13%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,3 +1,23 @@
+class Solution {
+    public int numSteps(String s) {
+        int steps = 0;
+        int carry = 0;
+
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = s.charAt(i) - '0';
+
+            if (bit + carry == 1) {
+                steps += 2;    
+                carry = 1;      
+            } else {
+                steps += 1;    
+               
+            }
+        }
+        return steps + carry;
+    }
+}
+
 // class Solution {
 //     public int numSteps(String s) {
 //         int l = s.length() - 1;
@@ -26,31 +46,3 @@
 //         return count;
 //     }
 // }
-
-class Solution {
-    public int numSteps(String s) {
-        Stack<Integer> st=new Stack<>();
-        for(char c:s.toCharArray()) st.push(c-'0');
-        int carry=0;
-        int steps=0;
-        while(st.size()>1){
-            if(st.peek()==1&&carry==1){ 
-                st.pop();
-                steps++;
-            }else if(st.peek()==1){ 
-                st.pop();
-                steps+=2;
-                carry=1;
-            }else if(st.peek()==0&&carry==1){
-                st.pop();
-                st.push(1);
-                carry=0;
-            }else{
-                st.pop();
-                steps++;
-            }
-        }
-        if(carry==1) steps++;
-        return steps;
-    }
-}
