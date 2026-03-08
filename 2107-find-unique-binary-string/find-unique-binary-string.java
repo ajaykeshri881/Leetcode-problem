fa170ed0eaaa94ec13f023e64f61f890af1e47c9
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-08 16:37:15 +0530

    Time: 1 ms (59.94%) | Memory: 43.4 MB (37.32%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -5,7 +5,6 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             res.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-
         return res.toString();
     }
 }
