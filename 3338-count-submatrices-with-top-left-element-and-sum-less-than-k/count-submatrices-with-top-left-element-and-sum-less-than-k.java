b9e2b49849149f883ba4cad74b2ad1e316bd8eb1
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-18 09:06:32 +0530

    Time: 5 ms (80.65%) | Memory: 160.9 MB (91.94%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,33 +1,30 @@
 class Solution {
     public int countSubmatrices(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        // prefix sum matrix
-        int[][] ps = new int[m][n];
-
-        // build prefix sum
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                ps[i][j] = grid[i][j];
-
-                if (i > 0) ps[i][j] += ps[i - 1][j];
-                if (j > 0) ps[i][j] += ps[i][j - 1];
-                if (i > 0 && j > 0) ps[i][j] -= ps[i - 1][j - 1];
+        int count=0,m=grid.length,n=grid[0].length;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                grid[i][j] += grid[i][j-1];
             }
         }
-
-        int count = 0;
-
-        // check each (i, j) as bottom-right
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (ps[i][j] <= k) {
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                grid[i][j] += grid[i-1][j];
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]<=k)
+                {
                     count++;
                 }
             }
         }
-
         return count;
     }
 }
