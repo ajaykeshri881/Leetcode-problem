6a44774319231a027350a2aeb89f793ed9363bf5
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-21 16:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (98.78%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -1,4 +1,23 @@
 class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+        for (int i = 0; i < k / 2; i++) {
+            int r1 = x + i;
+            int r2 = x + k - 1 - i;
+
+            for (int j = y; j < y + k; j++) {
+                int temp = grid[r1][j];
+                grid[r1][j] = grid[r2][j];
+                grid[r2][j] = temp;
+            }
+        }
+
+        return grid;
+    }
+}
+
+
+/* class Solution {
 
     private void swapRows(int[][] grid, int r1, int r2, int y, int k) {
         for (int j = 0; j < k; j++) {
@@ -16,7 +35,7 @@ class Solution {
 
         return grid;
     }
-}
+} */
 
 /*  class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
