1d87d12f0dcdb987553520e5ffb5a83e34e04ddd
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-21 15:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (78.37%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -1,21 +1,36 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        int[][] temp = new int[k][k];
 
-        // copy
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k / 2; i++) {
             for (int j = 0; j < k; j++) {
-                temp[i][j] = grid[x + i][y + j];
-            }
-        }
-
-        // reverse vertically
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                grid[x + i][y + j] = temp[k - 1 - i][j];
+                int temp = grid[x + i][y + j];
+                grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
+                grid[x + k - 1 - i][y + j] = temp;
             }
         }
 
         return grid;
     }
 }
+
+// class Solution {
+//     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+//         int[][] temp = new int[k][k];
+
+//         // copy
+//         for (int i = 0; i < k; i++) {
+//             for (int j = 0; j < k; j++) {
+//                 temp[i][j] = grid[x + i][y + j];
+//             }
+//         }
+
+//         // reverse vertically
+//         for (int i = 0; i < k; i++) {
+//             for (int j = 0; j < k; j++) {
+//                 grid[x + i][y + j] = temp[k - 1 - i][j];
+//             }
+//         }
+
+//         return grid;
+//     }
+// }
