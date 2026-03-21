2fa26aa7ff2e51b8b024a2f7acffd171cd19cd39
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-21 16:00:50 +0530

    Time: 1 ms (58.37%) | Memory: 47.2 MB (93.47%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -39,39 +39,39 @@ class Solution {
 } */
  
 
-// class Solution {
-//     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+/* class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
 
-//         for (int i = 0; i < k / 2; i++) {
-//             for (int j = 0; j < k; j++) {
-//                 int temp = grid[x + i][y + j];
-//                 grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
-//                 grid[x + k - 1 - i][y + j] = temp;
-//             }
-//         }
+        for (int i = 0; i < k / 2; i++) {
+            for (int j = 0; j < k; j++) {
+                int temp = grid[x + i][y + j];
+                grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
+                grid[x + k - 1 - i][y + j] = temp;
+            }
+        }
 
-//         return grid;
-//     }
-// }
+        return grid;
+    }
+} */
 
-// class Solution {
-//     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-//         int[][] temp = new int[k][k];
+/* class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        int[][] temp = new int[k][k];
 
-//         // copy
-//         for (int i = 0; i < k; i++) {
-//             for (int j = 0; j < k; j++) {
-//                 temp[i][j] = grid[x + i][y + j];
-//             }
-//         }
+        // copy
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                temp[i][j] = grid[x + i][y + j];
+            }
+        }
 
-//         // reverse vertically
-//         for (int i = 0; i < k; i++) {
-//             for (int j = 0; j < k; j++) {
-//                 grid[x + i][y + j] = temp[k - 1 - i][j];
-//             }
-//         }
+        // reverse vertically
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                grid[x + i][y + j] = temp[k - 1 - i][j];
+            }
+        }
 
-//         return grid;
-//     }
-// }
+        return grid;
+    }
+} */
