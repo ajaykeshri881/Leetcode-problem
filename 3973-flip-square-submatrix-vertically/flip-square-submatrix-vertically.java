ad83087b884fb34fad594574bf6fcb314de80279
Ajay Keshri <ajaykeshri881@gmail.com>
2026-03-21 16:00:33 +0530

    Time: 1 ms (58.37%) | Memory: 47.3 MB (78.37%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -1,17 +1,58 @@
 class Solution {
+
+    private void swapRows(int[][] grid, int r1, int r2, int y, int k) {
+        for (int j = 0; j < k; j++) {
+            int temp = grid[r1][y + j];
+            grid[r1][y + j] = grid[r2][y + j];
+            grid[r2][y + j] = temp;
+        }
+    }
+
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
 
         for (int i = 0; i < k / 2; i++) {
-            for (int j = 0; j < k; j++) {
-                int temp = grid[x + i][y + j];
-                grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
-                grid[x + k - 1 - i][y + j] = temp;
-            }
+            swapRows(grid, x + i, x + k - 1 - i, y, k);
         }
 
         return grid;
     }
 }
+
+/*  class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+        int top = x;
+        int bottom = x + k - 1;
+
+        while (top < bottom) {
+            for (int col = y; col < y + k; col++) {
+                int temp = grid[top][col];
+                grid[top][col] = grid[bottom][col];
+                grid[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+
+        return grid;
+    }
+} */
+ 
+
+// class Solution {
+//     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+//         for (int i = 0; i < k / 2; i++) {
+//             for (int j = 0; j < k; j++) {
+//                 int temp = grid[x + i][y + j];
+//                 grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
+//                 grid[x + k - 1 - i][y + j] = temp;
+//             }
+//         }
+
+//         return grid;
+//     }
+// }
 
 // class Solution {
 //     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
