42f6598c4506a2f6667d9baf6f9b09b55118bd13
Ajay Keshri <ajaykeshri881@gmail.com>
2026-04-05 14:29:04 +0530

    Time: 4 ms (99.05%) | Memory: 46.3 MB (25.09%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,30 +1,27 @@
-// class Solution {
-//     public boolean judgeCircle(String moves) {
-//         int x = 0, y = 0;
-
-//         for (char move : moves.toCharArray()) {
-//             if (move == 'U') y++;
-//             else if (move == 'D') y--;
-//             else if (move == 'R') x++;
-//             else if (move == 'L') x--;
-//         }
-
-//         return x == 0 && y == 0;
-//     }
-// }
-
-
 class Solution {
     public boolean judgeCircle(String moves) {
-        int up = 0, down = 0, left = 0, right = 0;
-
+        int x = 0, y = 0;
         for (char c : moves.toCharArray()) {
-            if (c == 'U') up++;
-            else if (c == 'D') down++;
-            else if (c == 'L') left++;
-            else if (c == 'R') right++;
+            if (c == 'U') y++;
+            else if (c == 'D') y--;
+            else if (c == 'R') x++;
+            else x--; // 'L'
         }
-
-        return up == down && left == right;
+        return x == 0 && y == 0;
     }
 }
+
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//         int up = 0, down = 0, left = 0, right = 0;
+
+//         for (char c : moves.toCharArray()) {
+//             if (c == 'U') up++;
+//             else if (c == 'D') down++;
+//             else if (c == 'L') left++;
+//             else if (c == 'R') right++;
+//         }
+
+//         return up == down && left == right;
+//     }
+// }
