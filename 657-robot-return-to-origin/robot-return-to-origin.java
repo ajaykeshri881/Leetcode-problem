4b0b7c8031fd5faebc275c26846665228ccd6d2a
Ajay Keshri <ajaykeshri881@gmail.com>
2026-04-05 14:27:46 +0530

    Time: 5 ms (68.18%) | Memory: 46.2 MB (25.09%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,14 +1,30 @@
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//         int x = 0, y = 0;
+
+//         for (char move : moves.toCharArray()) {
+//             if (move == 'U') y++;
+//             else if (move == 'D') y--;
+//             else if (move == 'R') x++;
+//             else if (move == 'L') x--;
+//         }
+
+//         return x == 0 && y == 0;
+//     }
+// }
+
+
 class Solution {
     public boolean judgeCircle(String moves) {
-        int x = 0, y = 0;
+        int up = 0, down = 0, left = 0, right = 0;
 
-        for (char move : moves.toCharArray()) {
-            if (move == 'U') y++;
-            else if (move == 'D') y--;
-            else if (move == 'R') x++;
-            else if (move == 'L') x--;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') up++;
+            else if (c == 'D') down++;
+            else if (c == 'L') left++;
+            else if (c == 'R') right++;
         }
 
-        return x == 0 && y == 0;
+        return up == down && left == right;
     }
 }
