75588a2e555ae5d9ae0e893aa06fbc25c869dc3a
Ajay Keshri <ajaykeshri881@gmail.com>
2026-05-11 22:14:02 +0530

    Time: 5 ms (77.69%) | Memory: 46.5 MB (78.62%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,14 +1,33 @@
+// class Solution {
+//     public int[] separateDigits(int[] nums) {
+//         List<Integer>ans=new ArrayList<>();
+//         for(int val:nums){
+//             List<Integer>cur=new ArrayList<>();
+//             while(val>0){
+//                 cur.add(val%10);
+//                 val/=10;
+//             }
+//             ans.addAll(cur.reversed());
+//         }
+//         return ans.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
+
 class Solution {
     public int[] separateDigits(int[] nums) {
-        List<Integer>ans=new ArrayList<>();
-        for(int val:nums){
-            List<Integer>cur=new ArrayList<>();
-            while(val>0){
-                cur.add(val%10);
-                val/=10;
+        ArrayList<Integer> supp = new ArrayList<>();
+        for(int i:nums){
+            String s = Integer.toString(i);
+            char [] arr = s.toCharArray();
+            for(char k:arr){
+                supp.add(k-'0');
             }
-            ans.addAll(cur.reversed());
         }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        int [] ans = new int [supp.size()];
+        for(int i=0;i<supp.size();i++){
+            ans[i] = supp.get(i);
+        }
+        return ans;
     }
 }
