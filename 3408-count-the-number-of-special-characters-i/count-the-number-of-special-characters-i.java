452eac515991cd4d00f11f1791932f95c326c15b
Ajay Keshri <ajaykeshri881@gmail.com>
2026-05-26 15:06:21 +0530

    Time: 2 ms (48.76%) | Memory: 43.6 MB (71.43%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,15 +1,35 @@
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         int[]lower=new int[26];
+//         int[]upper=new int[26];
+//         int ans=0;
+//         for(char c:word.toCharArray()){
+//             if(c>='a'&&c<='z')lower[c-'a']++;
+//             else upper[c-'A']++;
+//         }
+//         for(int i=0;i<26;i++){
+//             if(lower[i]!=0&&upper[i]!=0)ans++;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[]lower=new int[26];
-        int[]upper=new int[26];
-        int ans=0;
-        for(char c:word.toCharArray()){
-            if(c>='a'&&c<='z')lower[c-'a']++;
-            else upper[c-'A']++;
+        Set<Character> set = new HashSet<>();
+        
+        for (char c : word.toCharArray()) {
+            set.add(c);
         }
-        for(int i=0;i<26;i++){
-            if(lower[i]!=0&&upper[i]!=0)ans++;
+
+        int count = 0;
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (set.contains(c) && set.contains(Character.toUpperCase(c))) {
+                count++;
+            }
         }
-        return ans;
+
+        return count;
     }
 }
