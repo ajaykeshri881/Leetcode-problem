0b49a90eb1109f2959a44b2552d38f85e56767b3
Ajay Keshri <ajaykeshri881@gmail.com>
2026-06-16 21:05:55 +0530

    Time: 4 ms (76.47%) | Memory: 55.3 MB (48.53%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public String processStr(String s) {
         String result = "";
 
@@ -24,5 +24,33 @@ class Solution {
         }
 
         return result;
+    }
+} */
+
+
+
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isLowerCase(ch)) {
+                result.append(ch);
+            }
+            else if (ch == '*') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
+                }
+            }
+            else if (ch == '#') {
+                result.append(result.toString());
+            }
+            else if (ch == '%') {
+                result.reverse();
+            }
+        }
+
+        return result.toString();
     }
 }
