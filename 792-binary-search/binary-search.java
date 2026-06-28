3335449d7bd6e2727a695f91925df25baf8f8afe
Ajay Keshri <ajaykeshri881@gmail.com>
2026-06-28 17:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (68.50%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int lo=0;
+        int hi=arr.length-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]<target){
+                    lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        
+        }
+        return -1;
+    }
+}
